bf761e65893344ecb54edea5a9e44ca8cfe03287
Priyansh <pj3153@srmist.edu.in>
2026-02-16 08:56:27 +0530

    UC1: Print literal text OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp
+ * Prints OOPS to console
+ * @author Priyansh
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS");
+
+    }
+
+}
